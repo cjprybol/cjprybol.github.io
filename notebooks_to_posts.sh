ba6eb45c7da00999192ffb1b13d9bfcb9fdc8e88
Cameron Prybol <cameron.prybol@gmail.com>
2020-12-13 23:46:16 +0000

    Think I found the bad line in notebooks_to_posts.sh

--- notebooks_to_posts.sh
+++ notebooks_to_posts.sh
@@ -9,8 +9,8 @@ jupyter nbconvert --output-dir _posts --to markdown post_notebooks/*
 # converting jupyter notebooks to markdown files leaves an extra blank line at the top of the markdown file
 # which messes with the header metadata used by Jekyll. We can fix this by removing the first line of each
 # markdown file in place
-for filename in _posts/*; do
+# for filename in _posts/*; do
 	# echo $filename
 	# sed -i '1d' "$filename"
-	tail -n+2 "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
-done
+# 	tail -n+2 "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+# done
